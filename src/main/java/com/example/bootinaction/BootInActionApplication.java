4452f85efcfdb02e6e918845359c5d3fc3a31f0d
SEONMl <sm000117@naver.com>
2023-01-19 17:08:40 +0900

    init commit

---
+++ src/main/java/com/example/bootinaction/BootInActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootInActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootinaction/BootInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
