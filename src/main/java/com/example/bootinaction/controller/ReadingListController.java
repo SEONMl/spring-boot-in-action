f075c1214a9027210df6ee27e5dddf37af87b268
SEONMl <sm000117@naver.com>
2023-01-26 14:37:24 +0900

    feat: reader 타입 String -> Reader 객체로 변경

--- src/main/java/com/example/bootinaction/controller/ReadingListController.java
+++ src/main/java/com/example/bootinaction/controller/ReadingListController.java
@@ -1,8 +1,8 @@
 package com.example.bootinaction.controller;
 
-import java.util.List;
-
+import com.example.bootinaction.config.AmazonProperties;
 import com.example.bootinaction.entity.Book;
+import com.example.bootinaction.entity.Reader;
 import com.example.bootinaction.repository.ReadingListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,30 +10,34 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class ReadingListController {
 
-    private static final String reader = "craig";
-
     private ReadingListRepository readingListRepository;
+    private AmazonProperties amazonProperties;
 
     @Autowired
-    public ReadingListController(ReadingListRepository readingListRepository) {
+    public ReadingListController(ReadingListRepository readingListRepository, AmazonProperties amazonProperties) {
         this.readingListRepository = readingListRepository;
+        this.amazonProperties = amazonProperties;
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public String readersBooks(Model model) {
+    public String readersBooks(Reader reader, Model model) {
         List<Book> readingList = readingListRepository.findByReader(reader);
         if (readingList != null) {
             model.addAttribute("books", readingList);
+            model.addAttribute("reader", reader);
+            model.addAttribute("amazonID", amazonProperties.getAssociateId());
         }
         return "readingList";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String addToReadingList(Book book) {
+    public String addToReadingList(Reader reader, Book book) {
         book.setReader(reader);
         readingListRepository.save(book);
         return "redirect:/";
--- src/main/java/com/example/bootinaction/repository/ReadingListRepository.java
+++ src/main/java/com/example/bootinaction/repository/ReadingListRepository.java
@@ -3,8 +3,9 @@ package com.example.bootinaction.repository;
 import java.util.List;
 
 import com.example.bootinaction.entity.Book;
+import com.example.bootinaction.entity.Reader;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReadingListRepository extends JpaRepository<Book, Long> {
-    List<Book> findByReader(String reader);
+    List<Book> findByReader(Reader reader);
 }
